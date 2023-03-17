1f13e7732d1057c01379b9915359808d87c2b643
notryancho <ryancho1226@hotmail.com>
2023-03-17 13:34:45 -0400

    style the student card

--- southpark-university-app/src/components/StudentCard.vue
+++ southpark-university-app/src/components/StudentCard.vue
@@ -1,15 +1,15 @@
 <template>
   <div v-for="student in students" :key="student.id" class="student-card">
-    <h3>{{ student.name }}</h3>
-    <h3>{{ student.email }}</h3>
+    <h3 class="student-name">{{ student.name }}</h3>
+    <h3 class="student-email">{{ student.email }}</h3>
     <form @submit.prevent="onSubmit(student.id)">
       <label for="name">Name:</label>
       <input type="text" id="name" v-model="newName">
       <label for="email">Email:</label>
       <input type="text" id="email" v-model="newEmail">
-      <button type="submit">Save</button>
+      <button type="submit">Edit</button>
     </form>
-    <button @click="deleteOneStudent(student.id)">Delete</button>
+    <button class="delete-btn" @click="deleteOneStudent(student.id)">Delete</button>
   </div>
 </template>
 
@@ -49,8 +49,6 @@
   }
 </script>
 
-
-
 <style>
 .student-card {
   display: flex;
@@ -64,9 +62,59 @@
   border-radius: 10px;
   border-style: outset;
   border-color: #27AE60;
-  width: 180px;
-  height: 175px;
+  width: 300px;
+  height: 280px;
   margin: 20px;
+  padding: 20px;
   overflow: auto;
 }
+
+.student-name {
+  font-size: 28px;
+  margin-bottom: 10px;
+}
+
+.student-email {
+  font-size: 18px;
+  margin-bottom: 20px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+label {
+  margin-bottom: 5px;
+}
+
+input[type="text"] {
+  width: 100%;
+  margin-bottom: 10px;
+  padding: 8px;
+  border-radius: 5px;
+  border: none;
+  background-color: #f2f2f2;
+}
+
+button[type="submit"] {
+  background-color: #27AE60;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 8px;
+  cursor: pointer;
+}
+
+.delete-btn {
+  background-color: #c0392b;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 8px;
+  cursor: pointer;
+  margin-top: 10px;
+}
 </style>
+
